e801e83677afe7ff50a1ffb7c059b9a85c5942c6
tamusjroyce <TamusJRoyce@hotmail.com>
2018-01-18 13:49:22 -0600

    Update index.js
    Replaced setInterval with setTimeout. setInterval will queue up executions if the interval is shorter than the time it takes to run. This can have very bad consequences. Because of how setTimeout works, and it being cleared, this is not recursive.

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 var Promise = require('native-promise-only');
 
-module.exports = function poll(predicate, timeout, interval) {
-  if (typeof predicate !== 'function') return Promise.reject(new Error('predicate must be a function'));
-  if (typeof timeout !== 'number') return Promise.reject(new Error('timeout must be given'));
+// https://github.com/allain/promise-poll/blob/master/index.js - allain (Sept 18, 2015), 1/18/2018
+function poll(predicate, timeout, interval) {
+    if (typeof predicate !== 'function') return Promise.reject(new Error('predicate must be a function'));
+    if (timeout && typeof timeout !== 'number') return Promise.reject(new Error('timeout must be given as a number'));
 
-  return new Promise(function(resolve, reject) {
-    function check() {
-      var result = predicate();
-      if (result) {
-        clearInterval(intervalId);
-        clearInterval(timeoutId);
-        return resolve(result);
-      }
-    }
-    check();
+    return new Promise(function (resolve, reject) {
+        let intervalId;
+        let timeoutId;
 
-    var intervalId = setInterval(check, interval || 10);
-    var timeoutId = setTimeout(function() {
-      clearInterval(intervalId);
-      reject(new Error('timeout'));
-    }, timeout);
-  });
+        function check() {
+            let result = predicate();
+            if (result) {
+                if (intervalId) clearInterval(intervalId);
+                if (timeoutId) clearInterval(timeoutId);
+
+                return resolve(result);
+            }
+
+            if (intervalId) clearInterval(intervalId);
+            intervalId = setTimeout(check, interval || 10);
+        }
+        check();
+
+        if (timeout) {
+            timeoutId = setTimeout(function () {
+                clearInterval(intervalId);
+                reject(new Error('timeout'));
+            }, timeout);
+        }
+    });
 };
+
+if (typeof module == "undefined") {
+    window.module = {};
+}
+
+module.exports = function () {
+    getXMLHttpRequest = getxmlhttprequest;
+
+    let exports = new Object();
+    exports.poll = poll;
+
+    return exports;
+}
